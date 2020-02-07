16b2bf84b14717eeb95e1495dfbb2a87447c58b7
Tjark Kühl <tjark-kuehl@hotmail.com>
2020-02-07 14:46:26 +0100

    Cleanup

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,18 +1,10 @@
 <template>
     <div class="home">
         <img alt="Vue logo" src="../assets/logo.png" />
-        <HelloWorld msg="Welcome to Your Vue.js App" />
+        <h1>test</h1>
     </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
-export default {
-    name: 'index',
-    components: {
-        HelloWorld,
-    },
-};
+export default {};
 </script>
